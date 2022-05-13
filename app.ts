0b29ac38c57dc41bfb48d945551bbef51aaef456
rook-a <zhernovkov_konst@mail.ru>
2022-05-13 23:29:45 +0300

    refactor: правит сообщение ошибки

--- app.ts
+++ app.ts
@@ -1,20 +1,3 @@
-const courses = [
-  { name: 'Courses in England', prices: [0, 100] },
-  { name: 'Courses in Germany', prices: [500, null] },
-  { name: 'Courses in Italy', prices: [100, 200] },
-  { name: 'Courses in Russia', prices: [null, 400] },
-  { name: 'Courses in China', prices: [50, 250] },
-  { name: 'Courses in USA', prices: [200, null] },
-  { name: 'Courses in Kazakhstan', prices: [56, 324] },
-  { name: 'Courses in France', prices: [null, null] },
-];
-
-// data test
-
-const requiredRange1 = [null, 200];
-const requiredRange2 = [100, 350];
-const requiredRange3 = [200, null];
-
 const enum SortingByPriceType {
   Ascending = 'ascending',
   Descending = 'descending',
@@ -27,6 +10,23 @@ interface Course {
   name: string;
   prices: RequiredRange;
 }
+
+// data test
+
+const courses = [
+  { name: 'Courses in England', prices: [0, 100] },
+  { name: 'Courses in Germany', prices: [500, null] },
+  { name: 'Courses in Italy', prices: [100, 200] },
+  { name: 'Courses in Russia', prices: [null, 400] },
+  { name: 'Courses in China', prices: [50, 250] },
+  { name: 'Courses in USA', prices: [200, null] },
+  { name: 'Courses in Kazakhstan', prices: [56, 324] },
+  { name: 'Courses in France', prices: [null, null] },
+];
+
+const requiredRange1 = [null, 200];
+const requiredRange2 = [100, 350];
+const requiredRange3 = [200, null];
 
 // filter
 
@@ -83,6 +83,6 @@ const sortedByPrice = (courses: Course[], sortingType: SortingByPriceType) => {
         return b.prices[0] - a.prices[0];
       });
     default:
-      throw new Error(`Unexpecter sorting type ${sortingType}`);
+      throw new Error(`Unexpected sorting type ${sortingType}`);
   }
 };
