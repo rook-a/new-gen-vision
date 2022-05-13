8cec0a928778e6d694f47c5c8d8c2b42115b49ad
rook-a <zhernovkov_konst@mail.ru>
2022-05-13 23:26:36 +0300

    feat: добавит функцию сортировки

--- app.ts
+++ app.ts
@@ -1,10 +1,25 @@
-import courses from './data.json';
+const courses = [
+  { name: 'Courses in England', prices: [0, 100] },
+  { name: 'Courses in Germany', prices: [500, null] },
+  { name: 'Courses in Italy', prices: [100, 200] },
+  { name: 'Courses in Russia', prices: [null, 400] },
+  { name: 'Courses in China', prices: [50, 250] },
+  { name: 'Courses in USA', prices: [200, null] },
+  { name: 'Courses in Kazakhstan', prices: [56, 324] },
+  { name: 'Courses in France', prices: [null, null] },
+];
 
 // data test
 
 const requiredRange1 = [null, 200];
 const requiredRange2 = [100, 350];
 const requiredRange3 = [200, null];
+
+const enum SortingByPriceType {
+  Ascending = 'ascending',
+  Descending = 'descending',
+  Default = 'default',
+}
 
 type RequiredRange = (number | null)[];
 
@@ -15,7 +30,7 @@ interface Course {
 
 // filter
 
-const filteredWithPrice = (courses: Course[], requiredRange: RequiredRange) => {
+const filteredByPrice = (courses: Course[], requiredRange: RequiredRange) => {
   const [from, to] = requiredRange;
 
   if (from === null && to === null) {
@@ -46,4 +61,28 @@ const filteredWithPrice = (courses: Course[], requiredRange: RequiredRange) => {
 };
 
 // sort
-//...
+
+const sortedByPrice = (courses: Course[], sortingType: SortingByPriceType) => {
+  switch (sortingType) {
+    case SortingByPriceType.Default:
+      return courses;
+    case SortingByPriceType.Ascending:
+      return courses.sort((a, b) => {
+        if (a.prices[0] === null || b.prices[0] === null) {
+          return -1;
+        }
+
+        return a.prices[0] - b.prices[0];
+      });
+    case SortingByPriceType.Descending:
+      return courses.sort((a, b) => {
+        if (a.prices[0] === null || b.prices[0] === null) {
+          return -1;
+        }
+
+        return b.prices[0] - a.prices[0];
+      });
+    default:
+      throw new Error(`Unexpecter sorting type ${sortingType}`);
+  }
+};
